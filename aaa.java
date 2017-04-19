9a205fabf7d79b5c825adbd6cb4a2011e4b61f9a
zhangwei <18583875207@163.com>
2017-04-19 14:49:01 +0800

    aaa

---
+++ aaa.java
@@ -0,0 +1,79 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Aaa {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://start.firefoxchina.cn/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAaa() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.id("search-key")).clear();
+    driver.findElement(By.id("search-key")).sendKeys("haoweilai");
+    driver.findElement(By.xpath("//div[@id='engine-tip-show']/ul/li[3]")).click();
+    driver.findElement(By.linkText("好未来官方网站")).click();
+    driver.findElement(By.id("gywm")).click();
+    driver.findElement(By.id("search")).clear();
+    driver.findElement(By.id("search")).sendKeys("haoweilai");
+    driver.findElement(By.cssSelector("input.searchBtn")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
